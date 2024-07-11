1b929551f2b6c1f4004af1731e56f513d795d9ed
Your Name <you@example.com>
2024-07-11 16:34:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+const port = 5000;
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+let items = [];
+let workItems = [];
+let lastDate;
+
+
+app.get('/', function (req, res) {
+  const now = new Date();
+ 
+  
+  if (lastDate && now.getDate() !== lastDate.getDate()) {
+    items = [];
+    workItems = [];
+  }
+  lastDate = new Date(); 
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  const day = now.toLocaleDateString("en-US", options);
+
+  res.render('index.ejs', {
+    listTitle: day,
+    newListItems: items, 
+  });
+  // console.log(day);
+});
+
+
+app.post('/', (req, res) => {
+  const item = req.body.newItem;
+  if (req.body.list === "work"){
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+    res.redirect('/');
+  }
+});
+
+
+app.get("/work", (req, res) => {
+  res.render("index.ejs", {
+    listTitle: "Work List",
+    newListItems: workItems,
+  });
+});
+
+app.post("/work", (req, res) => {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+  
